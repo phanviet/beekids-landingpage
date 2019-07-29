4553f7e783f4518a26b4cc02748e386266266f1b
= <=>
2019-07-29 11:16:15 +0700

    done full-mobile

--- js/_reverse.js
+++ js/_reverse.js
@@ -7,15 +7,12 @@ card_header.addEventListener("click", function(){
 var active_click = document.getElementById("active-click");
 var tab_a =  document.getElementById("tab-A");
 var tab_b =  document.getElementById("tab-B");
-var k = 1
-// if(k){
-//   active_click.addEventListener('click', function(){
-//     tab_a.classList.toggle("active-click");
-//     k = 0
-//   })
-// } else{
-//     active_click.addEventListener('click', function(){
-//       tab_b.classList.toggle("active-click");
-//       k = 1
-//     })
-// }
+tab_a.addEventListener('click', function(){
+    tab_b.classList.remove("active-click");
+    tab_a.classList.add("active-click");
+})
+tab_b.addEventListener('click', function(){
+    tab_a.classList.remove("active-click");
+    tab_b.classList.add("active-click");
+})
+
