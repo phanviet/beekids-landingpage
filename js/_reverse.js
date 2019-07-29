3d478493740de40237ab6d0daca4db43550a474c
= <=>
2019-07-29 11:03:36 +0700

    review full-mobile

--- js/_reverse.js
+++ js/_reverse.js
@@ -3,3 +3,19 @@ var icon_reverse = document.getElementById("icon-reverse")
 card_header.addEventListener("click", function(){
   icon_reverse.classList.toggle("icon-chevron-down-reverse")
 })
+
+var active_click = document.getElementById("active-click");
+var tab_a =  document.getElementById("tab-A");
+var tab_b =  document.getElementById("tab-B");
+var k = 1
+// if(k){
+//   active_click.addEventListener('click', function(){
+//     tab_a.classList.toggle("active-click");
+//     k = 0
+//   })
+// } else{
+//     active_click.addEventListener('click', function(){
+//       tab_b.classList.toggle("active-click");
+//       k = 1
+//     })
+// }
